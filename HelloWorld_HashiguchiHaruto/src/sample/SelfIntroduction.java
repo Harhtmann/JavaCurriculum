83ee7b3dab48fa48573c07064aa917f9c2e728c1
Sunny <Sunny@TUFMK2>
2026-02-03 17:02:12 +0900

    Java3　導入　Selfintroduction

---
+++ HelloWorld_HashiguchiHaruto/src/sample/SelfIntroduction.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		/*変数宣言*/
+		String name = "橋口晴人";
+		int age = 26;
+		String hobby = "作品制作";
+
+		/*処理*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
