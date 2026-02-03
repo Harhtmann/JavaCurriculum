e7d4edffcae5c56e24bb67981608c5cd3a37ebab
Sunny <Sunny@TUFMK2>
2026-02-03 18:58:20 +0900

    Javaカリキュラム2.6まで完了

---
+++ HelloWorld_HashiguchiHaruto/src/curriculum/a/Question1.java
@@ -0,0 +1,29 @@
+package curriculum.a;
+
+public class Question1 {
+/*********************************************
+ * [概要]Javaカリキュラム2.6 Java基礎問題1 Q1
+ * [詳細]指定された変数の宣言と初期値の設定
+ *********************************************/
+	public static void main(String[] args) {
+		//変数byteNumの初期値設定
+		byte byteNum = 1;
+		//変数shortNumの初期値設定
+		short shortNum = 1;
+		//変数intNumの初期値設定
+		int intNum = 1;
+		//変数longNumの初期値設定
+		long longNum = 1;
+		//変数floatNumの初期値設定
+		float floatNum = 1;
+		//変数doubleNumの初期値設定
+		double doubleNum = 1;
+		//変数letterの初期値設定
+		char letter = 1;
+		//変数lettersの初期値設定
+		String letters = "letters";
+		//変数isBooleanの初期値設定
+		boolean isBoolean = false;
+	}
+
+}
---
+++ HelloWorld_HashiguchiHaruto/src/curriculum/a/Question2.java
@@ -0,0 +1,47 @@
+package curriculum.a;
+
+public class Question2 {
+/*************************************************
+ * [概要]Javaカリキュラム2.6 Java基礎問題1 Q1
+ * [詳細]Q1で宣言した変数へ指定された値を代入する
+ *************************************************/
+	public static void main(String[] args) {
+		//変数byteNumの初期値設定
+		byte byteNum = 1;
+		//変数byteNumに10を代入
+		byteNum = 10;
+		//変数shortNumの初期値設定
+		short shortNum = 1;
+		//変数shortNumに100を代入
+		shortNum = 100;
+		//変数intNumの初期値設定
+		int intNum = 1;
+		//変数intNumに1000を代入
+		intNum = 1000;
+		//変数longNumの初期値設定
+		long longNum = 1;
+		//変数longNumに10000を代入
+		longNum = 10000;
+		//変数floatNumの初期値設定
+		float floatNum = 1;
+		//変数floatNumに9.5fを代入
+		floatNum = 9.5f;
+		//変数doubleNumの初期値設定
+		double doubleNum = 1;
+		//変数doubleNumに10.5を代入
+		doubleNum = 10.5;
+		//変数letterの初期値設定
+		char letter = '1';
+		//変数letterに"a"を代入
+		letter = 'a';
+		//変数lettersの初期値設定
+		String letters = "letters";
+		//変数lettersに"ハロー"を代入
+		letters = "ハロー";
+		//変数isBooleanの初期値設定
+		boolean isBoolean = false;
+		//変数isBooleanにtrueを代入
+		isBoolean = true;
+	}
+
+}
