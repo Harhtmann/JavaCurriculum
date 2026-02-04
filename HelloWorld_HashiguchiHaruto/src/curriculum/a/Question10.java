bb0113a101bbdcd3925bedc3593537b8279ab362
Sunny <Sunny@TUFMK2>
2026-02-04 18:49:23 +0900

    Javaカリキュラム2.10　Java基礎問題1 Q13まで完了

--- HelloWorld_HashiguchiHaruto/src/curriculum/a/Question10.java
+++ HelloWorld_HashiguchiHaruto/src/curriculum/a/Question10.java
@@ -1,10 +1,21 @@
 package curriculum.a;
 
+/*************************************************
+* [概要]Javaカリキュラム2.9 Java基礎問題1 Q10
+* [詳細]int型の変数numをString型へ変換し、"得点50点"を
+* 　　　表示する。
+*************************************************/
+
 public class Question10 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		//変数numを宣言し初期化
+		int num = 50;
+		//変数numをintからString型へ変換する
+		String str = String.valueOf(num);
+		
+		//"得点50点"を表示する。
+		System.out.println( "得点:" + str );
 	}
 
 }
--- HelloWorld_HashiguchiHaruto/src/curriculum/a/Question8.java
+++ HelloWorld_HashiguchiHaruto/src/curriculum/a/Question8.java
@@ -2,14 +2,20 @@ package curriculum.a;
 
 /*************************************************
 * [概要]Javaカリキュラム2.9 Java基礎問題1 Q8
-* [詳細]
+* [詳細]priceをint型に変換し、整数価格:99と
+* 　　　コンソールに表示してください。
 *************************************************/
 
 public class Question8 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		//変数priceの宣言と初期化
+		double price = 99.99;
+		//変数priceをdoubleからintへ変換
+		int priceInt = (int) price;
+		
+		//"整数価格:99"と表示する。
+		System.out.println( "整数価格:" + priceInt );
 	}
 
 }
