0c6631b811d3e08674289bbe6113facaa31546db
Sunny <Sunny@TUFMK2.mshome.net>
2026-02-16 13:18:04 +0900

    Java　自己紹介プログラムを作成　完了

---
+++ HelloWorld_HashiguchiHaruto/src/curriculum/selfIntro/Main.java
@@ -0,0 +1,18 @@
+package curriculum.selfIntro;
+
+/**************************************************
+* Javaカリキュラム2.23　自己紹介プログラム
+**************************************************/
+
+public class Main {
+
+	public static void main(String[] args) {
+		//インスタンス化
+		Person person1 = new Person("鈴木太郎", 20, 1.7 ,60 ,20.76 ,1 );
+	    
+	    //自己紹介内容を出力
+	    person1.print();
+
+	}
+
+}
---
+++ HelloWorld_HashiguchiHaruto/src/curriculum/selfIntro/Person.java
@@ -0,0 +1,58 @@
+package curriculum.selfIntro;
+
+/**************************************************
+* Javaカリキュラム2.23　自己紹介プログラム
+**************************************************/
+
+public class Person {
+	  // インスタンスフィールドを定義
+	private String name;
+	private int age;
+	private double height;
+	private double weight;
+	private double bmi;
+	private int sum;
+	
+	  // コンストラクタを定義しインスタンスフィールドに値をセット
+	  Person( String name , int age , double height , double weight , double bmi , int sum ){
+		  this.name = name;
+		  this.age = age;
+		  this.height = height;
+		  this.weight = weight;
+		  this.bmi = bmi;
+		  this.sum = sum;
+	  }
+	  
+	  //getメソッド
+	  public String getName() {
+		  return this.name;
+	  }
+	  
+	  public int getAge() {
+		  return this.age;
+	  }
+	  
+	  public double getHeight() {
+		  return this.height;
+	  }
+	  
+	  public double getWeight() {
+		  return this.weight;
+	  }
+	  
+	  public double getBmi() {
+		  return this.bmi;
+	  }
+	  
+	  public double getSum() {
+		  return this.sum;
+	  }
+	  
+	  //自己紹介内容を出力
+	  public void print() {
+		    System.out.println( "名前は" + this.name + "です" );
+		    System.out.println( "年は" + this.age + "です" );
+		    System.out.println( "BMIは" + this.bmi + "です" );
+		    System.out.println( "合計" + this.sum + "です" );
+	  }
+}
